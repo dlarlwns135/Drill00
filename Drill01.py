5ffc8ca2eee945e4c2e241ef352cc6a0c97b870c
dlarlwns135 <dlarlwns135@naver.com>
2023-09-15 13:23:51 +0900

    Create Drill01.py
    일단 저장

---
+++ Drill01.py
@@ -0,0 +1,20 @@
+import turtle
+import random
+
+distance = 50
+d = distance
+
+def move_up():
+    turtle.setheading(90)
+    turtle.forward(d)
+
+def move_down():
+
+def move_right():
+
+def move_left():
+
+
+turtle.shape('turtle')
+
+turtle.onkey(move,
